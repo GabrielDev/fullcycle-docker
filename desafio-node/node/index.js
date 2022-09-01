95d49b72c9b6bb0108f7c7570f137d0bf033e5a0
Gabriel dos Santos <Gabriel.SantosLucena-ext@ab-inbev.com>
2022-09-01 14:47:11 -0300

    Servidor node com persistencia de usuário em MySQL, por trás do Nginx

---
+++ desafio-node/node/index.js
@@ -0,0 +1,31 @@
+import express from 'express';
+import { createUser, getUsers } from './users.js';
+
+const app = express();
+const HOST = '0.0.0.0';
+const PORT = process.env.PORT || 8080;
+
+app.get('/', async (req, res) => {
+  const { name } = req.query;
+
+  try {
+    await createUser(name);
+    const usersList = await getUsers();
+
+    const response = `
+        <main style="margin: 30px auto; width: 70%">
+            <section>
+                <h1>Full Cycle Rocks!</h1>
+            </section>
+            <section>${usersList}</section>
+        </main>
+        `;
+
+    res.status(200).send(response);
+  } catch (error) {
+    console.warn(error);
+    res.status(500);
+  }
+});
+
+app.listen(PORT, HOST, () => console.log(`🚀 Node APP rodando em: http://${HOST}:${PORT}`));
---
+++ desafio-node/node/users.js
@@ -0,0 +1,87 @@
+import mysql from 'mysql';
+
+const config = {
+  host: 'db',
+  user: 'root',
+  password: 'root',
+  database: 'nodedb',
+};
+
+const execQuery = async (sql, callback) => {
+  try {
+    const connection = mysql.createConnection(config);
+    const result = await connection.query(sql, callback);
+    connection.end();
+    return result;
+  } catch (error) {
+    console.warn(error);
+    throw error;
+  }
+};
+
+const getUserTable = (users = []) => {
+  let list = [`
+        <tr>
+            <td colspan="2">Nenhum registro encontrado</td>
+        </tr>
+    `];
+
+  if (users.length) {
+    list = users
+      .map(user => ({ ...user, created_at: new Date(user.created_at) }))
+      .map(user => `<tr><td align="center">${user.created_at.toLocaleString('pt-BR')}</td><td>${user.name}</td></tr>`);
+  }
+
+  return `
+    <h3>Cadastros</h3>
+    <table width="80%" border="1px" cellpadding="10px">
+      <thead>
+        <tr>
+          <th align="center" width="180px">
+            Criado em
+          </th>
+          <th>
+            Nome
+          </th> 
+        </tr>
+      </thead>
+      <tbody>
+        ${list.join('')}
+      </tbody>
+    </table>
+    `;
+};
+
+export const getUsers = async () => {
+  return new Promise((resolve, reject) => {
+    const sql = `SELECT * FROM people ORDER BY created_at DESC`;
+
+    execQuery(sql, (error, users) => {
+      if (error) reject(error);
+
+      resolve(getUserTable(users));
+    });
+  });
+};
+
+export const createTableIfNotExists = async () => {
+  const sql = `CREATE TABLE IF NOT EXISTS people (
+        id INT AUTO_INCREMENT PRIMARY KEY,
+        name VARCHAR(255), 
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    )`;
+
+  return execQuery(sql, error => {
+    if (error) throw error;
+  });
+};
+
+export const createUser = async (name = 'Full Cycle') => {
+  await createTableIfNotExists();
+
+  const sql = `INSERT INTO people(name) values("${name}")`;
+
+  return execQuery(sql, error => {
+    if (error) throw error;
+  });
+};
